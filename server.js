ed6e79b8e50782d7deee70ec7cda5ca2163100e1
Ryan Hayden <ryanhayden@Ryans-MacBook-Pro.local>
2017-11-17 10:56:25 -0600

    added .js

--- server.js
+++ server.js
@@ -26,3 +26,5 @@ app.use("/", router);
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+app.getElementsByTagName('')
